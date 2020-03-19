78c51e98ba0b5cd9413f76e4a711143a281558b9
Paul <prose0021@gmail.com>
2020-03-19 10:53:05 -0700

    hitting backend

--- index.js
+++ index.js
@@ -4,22 +4,29 @@ require("dotenv").config();
 const port = process.env.PORT || 8080;
 const app = express();
 const router = express.Router()
+const axios = require('axios');
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 
 // send the user to index html page inspite of the url
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
-router.get("/call", async (req, res) => {
-  const response = await axios.get(
+app.get("/call", async (req, res) => {
+  console.log(req)
+  await axios.get(
     `https://api.data.charitynavigator.org/v2/Organizations?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&categoryID=${req.category}&state=${req.state}&city=${req.fixedCity}`
-  );
-  res.send(response)
+  ).then( res =>
+    res.send(response)
+  ).catch( err =>
+    res.send(err)
+  )
 })
 
 app.listen(port);
+
+
 
 
